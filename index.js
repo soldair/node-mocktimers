379ea2b74e2a8a30d8ec3be841cc0e9a33c45149
Ryan Day <soldair@gmail.com>
2015-04-19 10:00:56 -0700

    standard

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ module.exports = function (delay) {
       var self = this
       var id = ++self.i
       var t = Date.now()
-      self.timers[id] = true;
+      self.timers[id] = true
       setImmediate(function () {
         delay(function () {
           t = Date.now() - t
-          if (t < dur) self.msoffset += (dur+1) - t
+          if (t < dur) self.msoffset += (dur + 1) - t
           if (self.timers[id]) fn()
-          delete self.timers[id];
+          delete self.timers[id]
         }, dur)
       })
       return id
@@ -33,12 +33,13 @@ module.exports = function (delay) {
       })()
       return id
     },
-    clear:function(id){
-      if(self.intervals[id]){
-        delete self.timers[self.intervals[id]];
-        delete self.intervals[id]; 
+    clear: function (id) {
+      var self = this
+      if (self.intervals[id]) {
+        delete self.timers[self.intervals[id]]
+        delete self.intervals[id]
       }
-      delete self.timers[id];
+      delete self.timers[id]
     },
     // lets you get a clock that maintains the contract of timeouts taking at least timeout ms
     now: function () {
--- test/timeout.js
+++ test/timeout.js
@@ -5,15 +5,15 @@ test('can setTimeout', function (t) {
   t.plan(4)
 
   var time = timers(function (fn, delay) {
-    t.ok(true,'called custom delay function')
+    t.ok(true, 'called custom delay function')
     fn() // callback with no delay. still async a la setImmediate
   })
 
-  t.equals(time.now(),Date.now())// true
+  t.equals(time.now(), Date.now())// true
 
   time.timeout(function () {
-    t.ok(1,'called user timeout');
-    t.ok(time.now() - 1000 >= Date.now(),'custom clock is correct.') // true
+    t.ok(1, 'called user timeout')
+    t.ok(time.now() - 1000 >= Date.now(), 'custom clock is correct.') // true
   }, 1000)
 
 })
